46981944296ed6adbd3f63795bfc76510e02c5c7
Jan Myszkier <jan@rocketweb.com>
2012-01-31 13:55:25 +0000

    removed zendx and added jquery from google hosting

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -64,17 +64,6 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 
         return $config;
     }
-
-
-    protected function _initViewHelpers()
-    {
-        //required for jquery integration
-        $view = new Zend_View();
-        $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer();
-        $view->addHelperPath('ZendX/JQuery/View/Helper/', 'ZendX_JQuery_View_Helper');
-        $viewRenderer->setView($view);
-        Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
-    }
-
+    
+    
 }
-
