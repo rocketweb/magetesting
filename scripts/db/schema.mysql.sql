dba90f25956401a397bbbe6c7f57fa5a575c1e66
Grzegorz Gąsiecki <grzegorz@rocketweb.com>
2012-02-06 11:11:39 +0000

    added column sample_data to queue table in schema and mwb

--- scripts/db/schema.mysql.sql
+++ scripts/db/schema.mysql.sql
@@ -49,6 +49,7 @@ CREATE  TABLE IF NOT EXISTS `queue` (
   `user_id` INT(11) NOT NULL ,
   `domain` VARCHAR(10) NOT NULL ,
   `instance_name` VARCHAR(100) NULL ,
+  `sample_data` INT(1) UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) ,
   INDEX `queue_to_version` (`version_id` ASC) ,
   INDEX `queue_to_user1` (`user_id` ASC) ,
