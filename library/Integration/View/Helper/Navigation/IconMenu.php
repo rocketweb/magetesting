2d5949815370ea0ffb50c9e0ad78b257933dd32c
Jan Myszkier <jan@rocketweb.com>
2012-03-27 12:43:10 +0000

    committing missing IconMenu helper that was uploaded to dev manually due to broken commit

---
+++ library/Integration/View/Helper/Navigation/IconMenu.php
@@ -0,0 +1,65 @@
+<?php
+// IconMenu.php
+class Zend_View_Helper_IconMenu extends Zend_View_Helper_Navigation_Menu
+{
+    protected $_iconPath = '/public/img';
+
+    public function iconMenu(Zend_Navigation_Container $container = null)
+    {
+        if (null !== $container) {
+            $this->setContainer($container);
+        }
+
+        return $this;
+    }
+
+    public function htmlify(Zend_Navigation_Page $page)
+    {
+        // get label and title for translating
+        $label = $page->getLabel();
+        $title = $page->getTitle();
+
+        // translate label and title?
+        if ($this->getUseTranslator() && $t = $this->getTranslator()) {
+            if (is_string($label) && !empty($label)) {
+                $label = $t->translate($label);
+            }
+            if (is_string($title) && !empty($title)) {
+                $title = $t->translate($title);
+            }
+        }
+
+        // get attribs for element
+        $attribs = array(
+            'id'     => $page->getId(),
+            'title'  => $title,
+            'class'  => $page->getClass()
+        );
+
+        // does page have a href?
+        if ($href = $page->getHref()) {
+            $element = 'a';
+            $attribs['href'] = $href;
+            $attribs['target'] = $page->getTarget();
+        } else {
+            $element = 'span';
+        }
+        
+        // dodanie ikonki
+        if (null !== $page->icon)
+        {
+	    if(strpos($page->icon,'.')){
+	      $icon = '<img src="'.$this->view->baseUrl().$this->_iconPath.'/'.$page->icon.'" alt="" /> ';
+	    } else {
+	      $icon = '<i class="'.$page->icon.'"></i>';
+	    }
+        } else {
+            $icon = '';
+        }
+
+        return '<' . $element . $this->_htmlAttribs($attribs) . '>'
+             . $icon
+             . $this->view->escape($label)
+             . '</' . $element . '>';
+    }
+}
