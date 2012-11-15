7fa31194d6362efc433ca393c55eaaf5ffb066e7
Jan Myszkier <jan@rocketweb.com>
2012-11-15 15:13:13 +0100

    adding pointer to queue table to fix ambiguity of the field in query

--- application/models/DbTable/Queue.php
+++ application/models/DbTable/Queue.php
@@ -16,7 +16,7 @@ class Application_Model_DbTable_Queue extends Zend_Db_Table_Abstract
                     ->join('instance', 'instance.id = queue.instance_id')
                     ->join('user', 'user.id = instance.user_id', 'login')
                     ->join('version', 'instance.version_id = version.id', 'version')
-                    ->where('server_id = ?',$server_id)
+                    ->where('queue.server_id = ?',$server_id)
                     ->where('task <> ?','MagentoDownload')
                     ->where('queue.status = ?','pending')
                     ->order(array('queue.id ASC', 'parent_id asc'));
@@ -29,7 +29,7 @@ class Application_Model_DbTable_Queue extends Zend_Db_Table_Abstract
                         ->join('instance', 'instance.id = queue.instance_id')
                     ->join('user', 'user.id = instance.user_id', 'login')
                     ->join('version', 'instance.version_id = version.id', 'version')
-                    ->where('server_id = ?',$server_id)
+                    ->where('queue.server_id = ?',$server_id)
                     ->where('task = ?','MagentoDownload')
                     ->where('queue.status = ?','pending')
                     ->order(array('queue.id ASC', 'parent_id asc'));
@@ -43,7 +43,7 @@ class Application_Model_DbTable_Queue extends Zend_Db_Table_Abstract
                         ->join('instance', 'instance.id = queue.instance_id')
                     ->join('user', 'user.id = instance.user_id', 'login')
                     ->join('version', 'instance.version_id = version.id', 'version')
-                    ->where('server_id = ?',$server_id)
+                    ->where('queue.server_id = ?',$server_id)
                     ->where('queue.status = ?','pending')
                     ->order(array('queue.id ASC', 'parent_id asc'));
             break;
