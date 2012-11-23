5a364f679dd77c8c896bf8241b3b0b3c2e55cc84
Jan Myszkier <jan@rocketweb.com>
2012-11-23 14:56:53 +0100

    fixing status auto update and moving javascript to user-dashboard.js

--- public/js/user-dashboard.js
+++ public/js/user-dashboard.js
@@ -60,3 +60,84 @@ $(document).ready(function(){
 	});
 
 });
+
+
+
+/* status auto-update start*/
+//define globals
+    var requests = [];
+    var domain;
+  
+    //refresh statuses
+    setInterval(updateStatuses,5000);
+
+function updateStatuses(){
+ 
+  $("div.accordion-toggle.installing").each(function(){
+  
+      var current_tr = $(this);
+              
+      //create 
+      domain = $(this).find(".instancedomain").val();
+      requests[domain];
+
+      //abort last request if it is still in queue
+      if(typeof requests[domain] !== "undefined"){
+      requests[domain].abort();
+      }
+      
+      //make new request
+      requests[domain] = $.ajax({
+            type: "POST",
+            url: "/queue/getstatus",
+            data: "domain=" + $(this).find(".instancedomain").val(),
+            dataType: "json",
+            success: function(json){
+                updateLabel(current_tr,json);
+            }
+      });
+  });
+}
+
+function updateLabel(row,new_status){
+    var labels = {
+        "pending": "Pending", /* shouldn\'t update to this status anyway */
+        "installing": "Installing",
+        "ready": "Ready",
+        "closed" : "Closed",
+        "error": "Error",
+        "installing-extension": "Installing Extension",
+        "installing-magento": "Installing Magento",
+        "installing-samples" : "Installing Sample Data",
+        "installing-user" : "Installing User",
+        "installing-files" : "Installing Files",
+        "installing-data" : "Installing Data/Database"
+    }
+    
+        if (new_status=="ready"){
+            //remove progress bar and just insert label
+            row.find("span.bar").parent().parent().html("<span class=\"span2 progress progress-success progress-striped pull-right\" style=\"height:12px; margin-bottom: 0px\"><span class=\"bar\" style=\"width: 100%;\"></span></span><span class=\"pending pull-right statusHolder\" rel=\"popover\" data-placement=\"bottom\" data-original-title=\"A Title\" data-content=\"\">" + labels[new_status] + "</span>");
+            location.reload();
+        } else if(new_status != "pending") {
+            //update progress bar status
+            row.find("span.statusHolder").html("" + labels[new_status] + "");
+        } else {
+            //update pending time counter
+        $.ajax({
+            type: "POST",
+            url: "/queue/getminutesleft",
+            data: "domain=" + row.find(".instancedomain").val(),
+            dataType: "json",
+            success: function(json){
+                var time = '. $this->timeExecution .'*json;
+                row.find("span.statusHolder").html("In Queue - " + time + " minutes left");
+            }
+      });
+        }
+        
+        //remove installing label now when everything is updated
+        if (new_status.substr(0,10) != "installing" && new_status != "pending"){
+            row.removeClass("installing");
+        }
+}
+/* status auto-update end*/
