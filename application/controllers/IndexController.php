dc743618458c293e877fde1090c0a5a73466555b
Grzegorz Gasiecki <grzegorz@rocketweb.com>
2012-11-27 11:31:04 +0100

    removed dummy dump

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,7 +10,6 @@ class IndexController extends Integration_Controller_Action
 
     public function indexAction()
     {
-        var_dump('initsql');
         // action body
         $auth = $this->auth->getIdentity();
         $this->view->user_logged = isset($auth->id) AND $auth->id ? true : false;
