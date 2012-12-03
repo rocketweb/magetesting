6676a53022014b950ccf231ba74b138dedeadb5b
Jan Myszkier <jan@rocketweb.com>
2012-12-03 11:28:34 +0100

    basic code for rollback

--- application/models/DbTable/Revision.php
+++ application/models/DbTable/Revision.php
@@ -14,4 +14,20 @@ class Application_Model_DbTable_Revision extends Zend_Db_Table_Abstract
                        ->order('r.id DESC');
         return $this->fetchAll($select);
     }
+    
+    /**
+     * Used only in revert
+     * @param type $instance_id
+     * @return type
+     */
+     
+    public function getPreLastForInstance($instance_id)
+    {
+        $select = $this->select()
+                       ->from(array('r' => $this->_name), array('r.id', 'r.comment', 'r.filename', 'r.db_before_revision', 'r.hash'))
+                       ->where('r.instance_id = ?', $instance_id)
+                       ->order('r.id DESC')
+                       ->limit(1, 1);
+        return $this->fetchRow($select);
+    }
 }
