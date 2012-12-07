79ef9bea83edaca44da36ff954fef6919ac0ea0b
Piotr Skonieczny <piotr@rocketweb.com>
2012-12-07 10:01:50 +0100

    Add Custom store - radio buttons JavaScript

--- public/js/queue-addcustom.js
+++ public/js/queue-addcustom.js
@@ -21,7 +21,24 @@ $(document).ready(function(){
     });
     
     
-    $('div.input-radio .input-radio-option').click(function(){
-       console.log($(this));
+    var defaultRadioAlert = 'alert alert-info';
+    
+    $('div.input-radio .input-radio-option, div.input-radio .input-radio-button').click(function(){
+        var $this = $(this);
+        var $radioGroup = $this.parent().parent();
+        
+        // Update alerts
+        $radioGroup.find('.input-radio-alert')
+            .removeClass(defaultRadioAlert)
+            .find('input').css('width', '');
+        
+        $this.parent().find('.input-radio-alert')
+            .addClass(defaultRadioAlert)
+            .find('input').css('width', '100%');
+        
+        // Update radio buttons
+        $radioGroup.find('.input-radio-button input').removeAttr('checked');
+        $this.parent().find('.input-radio-button input').attr('checked', 'checked');
+        
     });
 });
