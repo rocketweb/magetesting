e923274916fdcb91b9bf87641afc87d5e5c9d188
Grzegorz Gasiecki <grzegorz@rocketweb.com>
2012-12-14 17:19:59 +0100

    fixed overwritten instance to store change

--- application/models/Plan.php
+++ application/models/Plan.php
@@ -192,7 +192,7 @@ class Application_Model_Plan {
             'stores' => $this->getStores(),
             'ftp_access' => $this->getFtpAccess(),
             'phpmyadmin_access' => $this->getPhpmyadminAccess(),
-            'can_add_custom_instance' => $this->getCanAddCustomInstance(),
+            'can_add_custom_instance' => $this->getCanAddCustomStore(),
             'billing_period' => $this->getBillingPeriod(),
             'price'     => $this->getPrice(),
             'is_hidden' => $this->getIsHidden()
