8a20d54e5c00084e56b2f28534c0744ca1381ac3
Wojciech Naruniec <wojtek@rocketweb.com>
2012-12-22 08:14:23 +0100

    Enabled tooltip on deployment popup.

--- public/js/custom.js
+++ public/js/custom.js
@@ -82,6 +82,7 @@ $(document).ready(function () {
                            if(typeof html == 'string' && html.length) {
                                $deploy_table_body.append(html.replace(/(Adding )([^\(]*[^\(\s])( \(.*\))*\</ig, '$2<'));
                            }
+                           $(".btn.request-deployment.disabled").tooltip({placement: 'right'});
                        }
                     });
                 }
