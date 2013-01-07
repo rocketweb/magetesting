88c82ae78d1870db4880f01a34e79e1fad597611
Jan Myszkier <jan@rocketweb.com>
2013-01-07 15:43:01 +0100

    force protocol on getcheme to be http

--- application/views/helpers/GetScheme.php
+++ application/views/helpers/GetScheme.php
@@ -9,14 +9,16 @@ class Zend_View_Helper_GetScheme extends Zend_View_Helper_ServerUrl
     public function GetScheme()
     {
         switch (true) {
-            case (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] === true)):
+            /*currently forced to be http*/
+            /*case (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] === true)):
             case (isset($_SERVER['HTTP_SCHEME']) && ($_SERVER['HTTP_SCHEME'] == 'https')):
             case (isset($_SERVER['SERVER_PORT']) && ($_SERVER['SERVER_PORT'] == 443)):
                 $scheme = 'https';
-                break;
+                break;*/
             default:
             $scheme = 'http';
         }
+       
         return $scheme.'://';
     }
 }
