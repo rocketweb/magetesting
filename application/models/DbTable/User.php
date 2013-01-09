6b89c96b65ead923142c4931983baa13a8d5490a
Jan Myszkier <jan@rocketweb.com>
2013-01-09 12:08:24 +0100

    fixing query

--- application/models/DbTable/User.php
+++ application/models/DbTable/User.php
@@ -25,7 +25,7 @@ class Application_Model_DbTable_User extends Zend_Db_Table_Abstract
         $select = $this->select()
                         ->setIntegrityCheck(false)
                         ->from(array('u' => $this->_name), array())
-                        ->where('se.id = ?', (int)$extensionId)
+                        ->where('se.extension_id = ?', (int)$extensionId)
                         ->where('u.id = ?', (int)$userId)
                         ->joinLeft(array('s' => 'store'), 'u.id = s.user_id', 's.id as store')
                         ->joinLeft(array('se' => 'store_extension'), 'se.store_id = s.id', array('se.reminder_sent', 'se.braintree_transaction_id'));
