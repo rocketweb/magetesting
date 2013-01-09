a97a8d9d8064dd260ae0b9209b4bedede265c0e7
Jan Myszkier <jan@rocketweb.com>
2013-01-09 13:52:51 +0100

    fix of file extension

--- scripts/db/mysql-update-1.8.7.php
+++ scripts/db/mysql-update-1.8.7.php
@@ -1,3 +1,7 @@
 <?php
 
 $sql[]="ALTER TABLE extension_category ADD COLUMN logo VARCHAR(50) NULL DEFAULT NULL";
+
+$sql[]="UPDATE extension_category SET logo='user-experience.jpg' WHERE id=1";
+$sql[]="UPDATE extension_category SET logo='site-management.jpg' WHERE id=2";
+$sql[]="UPDATE extension_category SET logo='promotion.jpg' WHERE id=3";
