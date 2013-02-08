e11ab4bba06b40f9d96217b23cac7dc5ea41a497
Jan Myszkier <jan@rocketweb.com>
2013-02-08 16:49:47 +0100

    additional removal lines for elements we've added on the way

--- application/models/Task/Magento.php
+++ application/models/Task/Magento.php
@@ -74,6 +74,9 @@ extends Application_Model_Task {
         
     }
     
+    /**
+     * @deprecated: since we're having per-user-vhosts, we don't need symlinks
+     */
     protected function _createSymlink(){
         $domain = $this->_storeObject->getDomain();
         $this->logger->log('Added symbolic link for store directory.', Zend_Log::INFO);
