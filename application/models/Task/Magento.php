e0f84f1be8849618ee062a0a9f1f3e2ff4d5e056
Jan Myszkier <jan@rocketweb.com>
2013-02-14 15:35:58 +0100

    fixed semicolon and newline

--- application/models/Task/Magento.php
+++ application/models/Task/Magento.php
@@ -289,8 +289,8 @@ extends Application_Model_Task {
         exec('sudo a2ensite '.$this->_dbuser.'.'.$this->_serverObject->getDomain());
         exec('sudo /etc/init.d/apache2 reload');
         
-        $redirector = '<?php 
-        header("Location: ' . $this->config->magento->storeUrl . '/user/dashboard")';
+        $redirector = '<?php '.
+        PHP_EOL.'header("Location: ' . $this->config->magento->storeUrl . '/user/dashboard");';
         $fileLocation = '/home/'.$this->config->magento->userprefix . $this->_dbuser.'/public_html/index.php';
         file_put_contents($fileLocation, $redirector);
         exec('sudo chmod a+x '.$fileLocation);
