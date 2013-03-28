289f5223f28ce55facbfc02dbad8823c305c1324
Jan Myszkier <jan@rocketweb.com>
2013-03-28 16:43:09 +0100

    help controller to handle help pages

---
+++ application/controllers/HelpController.php
@@ -0,0 +1,37 @@
+<?php
+
+class HelpController extends Integration_Controller_Action
+{
+
+    public function init()
+    {
+        $this->_helper->sslSwitch(false);
+    }
+
+    public function indexAction() 
+    {
+        //index action
+    }
+    
+    public function categoryAction()
+    {
+        $request = $this->getRequest();
+        $category = $request->getParam('category');
+
+        if (file_exists(APPLICATION_PATH . '/views/scripts/help/' . $category . '/' . $page)) {
+            $this->renderScript('help/' . $category . '.phtml');
+        }
+    }
+    
+    public function pageAction()
+    {
+        $request = $this->getRequest();
+        $category = $request->getParam('category');
+        $page = $request->getParam('page');
+
+        if (file_exists(APPLICATION_PATH . '/views/scripts/help/' . $category . '/' . $page.'.phtml')) {
+            $this->renderScript('help/' . $category . '/' . $page . '.phtml');
+        }
+    }
+    
+}
