6e5a7521b4044325bad88120cf9301a65629e97a
Grzegorz Gasiecki <grzegorz@rocketweb.com>
2013-05-09 11:50:47 +0200

    added mapping api request fields to form equivalents, removed db_hourly and assumed it equals 0

--- application/modules/api/controllers/StoreController.php
+++ application/modules/api/controllers/StoreController.php
@@ -2,6 +2,18 @@
 
 class Api_StoreController extends Integration_Controller_Action
 {
+    protected $_response_object = array(
+            'type' => 'error',
+            'message' => 'Wrong REST api call'
+    );
+
+    public function init()
+    {
+        parent::init();
+        $this->_helper->layout()->disableLayout();
+        $this->_helper->viewRenderer->setNoRender(true);
+    }
+
     public function indexAction()
     {
         /* render wrong rest call message */
@@ -9,7 +21,7 @@ class Api_StoreController extends Integration_Controller_Action
     }
     public function getAction()
     {
-        $this->render('rest-error');
+        $this->getResponse()->setBody(json_encode($this->_response_object));
     }
     public function putAction()
     {
@@ -20,10 +32,6 @@ class Api_StoreController extends Integration_Controller_Action
         $this->getAction();
     }
 
-    protected $_response_object = array(
-            'type' => 'error',
-            'message' => 'Wrong REST api call'
-    );
     public function postAction()
     {
         $username = $this->getParam('username');
@@ -37,8 +45,6 @@ class Api_StoreController extends Integration_Controller_Action
             }
         }
 
-        $this->_helper->layout()->disableLayout();
-        $this->_helper->viewRenderer->setNoRender(true);
         $this->getResponse()->setBody(json_encode($this->_response_object))->setHeader('Content-Type', 'text/json');
     }
     protected function _authenticate($username, $apikey, Application_Model_User $userObject)
@@ -78,7 +84,12 @@ class Api_StoreController extends Integration_Controller_Action
     {
         $this->auth->getStorage()->write((object)$userObject->__toArray());
         $form = new Application_Form_StoreAddCustom();
-        $path_type = $this->_getParam('input-radio');
+
+        // change api field names to add-custom form fields
+        $params = $this->_mapAddStoreFormFields($this->_getAllParams());
+
+        // check whether use path_backup or path_store to download store
+        $path_type = $this->_getParam('backup_type');
         if(!in_array($path_type, array('remote_path', 'file'))) {
             $path_type = 'remote_path';
         }
@@ -90,10 +101,24 @@ class Api_StoreController extends Integration_Controller_Action
             $form->custom_file->setRequired(true);
         }
 
-        if($form->isValid($this->getRequest()->getParams())) {
-            // fix version number
-            $form->version->setValue(substr($form->version->getValue(),2));
+        $params['do_hourly_db_revert'] = 0;
 
+        // find version id
+        if(isset($params['version']) && isset($params['edition'])) {
+            $params['version'] = $params['version'];
+            $versionModel = new Application_Model_Version();
+            $versions = $versionModel->getAllForEdition($params['edition']);
+            if(is_array($versions)) {
+                foreach($versions as $row) {
+                    if($row['version'] == $params['version'] && $row['edition'] == $params['edition']) {
+                        $params['version'] = $params['edition'].$row['id'];
+                    }
+                }
+            }
+        }
+        if($form->isValid($params)) {
+            // fix version id
+            $form->version->setValue(substr($form->version->getValue(),2));
             try {
                 $storeModel = new Application_Model_Store();
                 $storeModel->setOptions($form->getValues())
@@ -189,5 +214,29 @@ class Api_StoreController extends Integration_Controller_Action
             $this->_response_object['message'] = 'Invalid data';
         }
         $this->auth->getStorage()->clear();
+    }
+
+    protected function _mapAddStoreFormFields($array)
+    {
+        $map = array(
+            'name'         => 'store_name',
+            'host'         => 'custom_host',
+            'login'        => 'custom_login',
+            'password'     => 'custom_pass',
+            'path_sql'     => 'custom_sql',
+            'path_store'   => 'custom_remote_path',
+            'path_backup'  => 'custom_file',
+            'port'         => 'custom_port',
+            'protocol'     => 'custom_protocol'
+        );
+        $mapped_array = array();
+        foreach($array as $key => $value) {
+            if(isset($map[$key])) {
+                $mapped_array[$map[$key]] = $value;
+            } else {
+                $mapped_array[$key] = $value;
+            }
+        }
+        return $mapped_array;
     }
 }
