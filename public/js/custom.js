f49b07e0c985298b66df40456cf2fe722443da47
Wojciech Naruniec <wojtek@rocketweb.com>
2013-06-07 16:40:55 +0200

    Added first hekp pages and added misc tweaks.

--- public/js/custom.js
+++ public/js/custom.js
@@ -664,11 +664,18 @@ $(document).ready(function () {
     
     $("#iconmenu-help, #context-help i").click(function () {
         var $helpBlock = $('#wrap-context-help');
+
+        if ($helpBlock.length == 0) {
+            return;
+        }
+
         $helpBlock.animate({
             right: parseInt($helpBlock.css('right'), 10) == 0 ?
                 -$helpBlock.outerWidth() :
                 0
         });
+
+        return false;
     });
 
     /* Store extensions status updater */
