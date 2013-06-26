2aaf0b5cc406d2133bcdf2eb63d147c35cdf346a
Grzegorz Gasiecki <grzegorz@rocketweb.com>
2013-06-26 13:54:52 +0200

    fixed mysql error with too many current_timestamp columns

--- scripts/db/mysql-update-1.0.5.php
+++ scripts/db/mysql-update-1.0.5.php
@@ -1,5 +1,5 @@
 <?php
 
 $sql[] = '
-ALTER TABLE  `queue` ADD `next_execution_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+ALTER TABLE  `queue` ADD `next_execution_time` timestamp NOT NULL DEFAULT \'0000-00-00 00:00:00\'
 ';
