7f83125f238603fd8113c108913009657884edf0
Grzegorz Gasiecki <golaod@gmail.com>
2013-09-07 20:00:01 +0200

    added basic model which be bridge between installation task and ioncube server

---
+++ application/models/Ioncube/Encode.php
@@ -0,0 +1,76 @@
+<?php
+
+class Application_Model_Ioncube_Encode
+{
+    protected $_config;
+    protected $_storeDir;
+    protected $_cli;
+    protected $_log;
+
+    public function setup($storeDir, $config, $log = null)
+    {
+        $this->_storeDir = trim($storeDir, '/');
+        $this->_config = $config;
+
+        if($log instanceof Zend_Log) {
+            $this->_log = $log;
+        }
+        return $this;
+    }
+
+    protected function cli($kit = '')
+    {
+        if(!$this->_cli) {
+            $this->_cli = new RocketWeb_Cli();
+
+            if($this->_log) {
+                $this->_cli->setLogger($log);
+                $this->_cli->enableLogging(true);
+            }
+        }
+        if($kit) {
+            return $this->_cli->kit($kit);
+        }
+        return $this->_cli;
+    }
+
+    public function process()
+    {
+        $this->_packDecodedEnterprise();
+        $this->_encodeEnterprise();
+        $this->_unpackEncodedEnterprise();
+
+        $this->cli()->call();
+    }
+
+    protected function _packDecodedEnterprise()
+    {
+        $this->cli()->append(
+            $this->cli('tar')->pack('-', $this->_storeDir.'/app/code/core/Enterprise')->isCompressed()
+        );
+    }
+    protected function _encodeEnterprise()
+    {
+        $ssh = $this->cli('ssh');
+        $ioncube = $this->_config->ioncubeEncoder;
+
+        $ssh->connect(
+            $ioncube->user,
+            $ioncube->pass,
+            $ioncube->host,
+            $ioncube->port
+        );
+
+        $ssh->remoteCall('put here proper code or create new kit');
+
+        $this->cli()->pipe(
+            $ssh
+        );
+    }
+    protected function _unpackEncodedEnterprise()
+    {
+        $this->cli()->pipe(
+            $this->cli('tar')->unpack('-', $this->_storeDir.'/app/code/core/Enterprise')
+        );
+    }
+}
