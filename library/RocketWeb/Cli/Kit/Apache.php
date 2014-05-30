a02a63a0fcc4cbef899b76c335e1aab0c8e9ff94
Wojciech Naruniec <wojtek@rocketweb.com>
2014-05-30 18:27:54 +0200

    Improved a2ensite command call.

--- library/RocketWeb/Cli/Kit/Apache.php
+++ library/RocketWeb/Cli/Kit/Apache.php
@@ -5,11 +5,11 @@ class RocketWeb_Cli_Kit_Apache
 {
     public function enableSite($site)
     {
-        return $this->append('a2ensite ?', $site);
+        return $this->append('/usr/sbin/a2ensite ?', $site);
     }
 
     public function disableSite($site)
     {
-        return $this->append('a2dissite ?', $site);
+        return $this->append('/usr/sbin/a2dissite ?', $site);
     }
 }
