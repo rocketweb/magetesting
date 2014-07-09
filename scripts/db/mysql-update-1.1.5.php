1c0107545f059cf202a024fcb240df626bbbbaef
Gregor Pollak <gregor@pollak.si>
2014-07-09 22:05:08 +0200

    Updated mysql-update script [#4]

--- scripts/db/mysql-update-1.1.5.php
+++ scripts/db/mysql-update-1.1.5.php
@@ -3,10 +3,11 @@ $sql[] = '
 CREATE TABLE IF NOT EXISTS `store_conflict` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `store_id` int(11) NOT NULL,
-`module` varchar(255) COLLATE utf8_bin NOT NULL,
+`type` varchar(255) COLLATE utf8_bin NOT NULL,
 `class` varchar(255) COLLATE utf8_bin NOT NULL,
 `rewrites` text COLLATE utf8_bin NOT NULL,
-`ignore` tinyint(1) NOT NULL,
+`loaded` text COLLATE utf8_bin NOT NULL,
+`ignore` TINYINT(1) NOT NULL DEFAULT \'0\',
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;';
 
